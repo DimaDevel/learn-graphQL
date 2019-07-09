01890b6879ff7eceebfa59d63ac812f1f7fbc348
DimaDevel <dima.ivanov.dev@gmail.com>
2019-07-10 00:22:34 +0300

    Initial commit.

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/dima-ivanov-67cab8/hackernews_node/dev`
+});
+exports.prisma = new exports.Prisma();
