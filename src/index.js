0d4af26b0dd5b8a51fa7b47155e1a0c306965548
DimaDevel <dima.ivanov.dev@gmail.com>
2019-07-11 00:30:54 +0300

    Added prisma lib.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
 
-
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}, {
-    id: 'link-1',
-    url: 'www.google.com',
-    description: 'this is test sample.'
-}];
-
-let idCount = links.length;
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-        link: (parent, { id }) => {
-        	return `this is id -> ${id}`;
+        feed: (root, args, context, info) => {
+            return context.prisma.links();
         },
     },
      Mutation:{
-    	post: (parent, args) => {
-    		const link = {
-    			id: `link-${idCount++}`,
-    			description: args.description,
-    			url: args.url,
-    		};
-    		links.push(link);
-
-    		return link;
+    	post: (root, args, context) => {
+    		return context,prisma.createLink({
+                url: args.url,
+                description: args.description,
+            });
     	},
-    	// updateLink: (parent, args) => {
-
-    	// },
-    	// deleteLink: (parent, args) => {
-
-    	// }
     },
 };
 
 const server = new GraphQLServer({
     typeDefs: './schema.graphql',
     resolvers,
+    context: { prisma },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
