b0d9239b15eab8fddb6b6d774dcd09eeb8acde07
DimaDevel <dima.ivanov.dev@gmail.com>
2019-07-16 00:43:19 +0300

    Added utils.

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({
-    typeDefs: './schema.graphql',
+    typeDefs: './src/schema.graphql',
     resolvers,
     context: { prisma },
 });
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,7 @@
+const bcrypt = require('bcryptjs');
+
+async function signup(parent, args, context, info) {
+    const password = await bcrypt.hash(args.password, 10);
+    const user = await context.prisma.createUser({ ...args, password });
+    const token = jwt.sing();
+}
